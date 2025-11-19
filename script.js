c3078c3746ae84e11a721fa7fc007890aaedcec8
amin zd <aminzineddine2@gamil.com>
2025-11-19 09:26:22 +0100

    initialiser mon fichier js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+const employeZone = {
+    reception: ["receptionist", "manager"],
+    server: ["technician", "manager"],
+    security: ["security", "manager"],
+    conference: ["receptionist", "technician", "security", "manager", "cleaner", "other"],
+    staff: ["receptionist", "technician", "security", "manager", "cleaner", "other"],
+    archive: ["receptionist", "technician", "security", "manager", "other"]
+};
+
+const capacityZone = {
+    reception: 2,
+    server: 3,
+    security: 2,
+    conference: 10,
+    staff: 8,
+    archive: 2
+};
+
+
+const principalZones = ["reception", "server", "security"];
+
+
